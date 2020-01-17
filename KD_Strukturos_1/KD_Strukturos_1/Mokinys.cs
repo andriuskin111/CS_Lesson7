068cd9b6bcc9d159554740381d7b7e72f6106d2a
andriuskin111 <andriuskin@gmail.com>
2020-01-17 20:30:28 +0200

    prideta struktura ir konstruktorius

---
+++ KD_Strukturos_1/KD_Strukturos_1/Mokinys.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KD_Strukturos_1
+{
+    struct Mokinys
+    {
+        public string vardas;
+        public string pavarde;
+        public string kursoPradziosData;
+        public string kursoPabaigosData;
+
+        public Mokinys(string vard, string pavard, string kursoPrad, string kursoPab)
+        {
+            vardas = vard;
+            pavarde = pavard;
+            kursoPradziosData = kursoPrad;
+            kursoPabaigosData = kursoPab;
+        }
+        public void ParodytiDuomenis()
+        {
+            Console.WriteLine($"Vardas: {vardas}\n" +
+                $"Pavarde: {pavarde}\n" +
+                $"Kurso pradzios data: {kursoPradziosData}\n" +
+                $"Kurso pabaigos data: {kursoPabaigosData}");
+        }
+    }
+}
--- KD_Strukturos_1/KD_Strukturos_1/Program.cs
+++ KD_Strukturos_1/KD_Strukturos_1/Program.cs
@@ -10,6 +10,16 @@ namespace KD_Strukturos_1
     {
         static void Main(string[] args)
         {
+            string kursoPradziosData = Convert.ToString(new DateTime(2019, 12, 10).ToShortDateString());
+            string kursoPabaigosData = Convert.ToString(new DateTime(2020, 02, 25).ToShortDateString());
+
+            Mokinys mokinys1 = new Mokinys("Andrius", "Geniusas", kursoPradziosData, kursoPabaigosData);
+            Mokinys mokinys2 = new Mokinys("Vasia", "Petrov", kursoPradziosData, kursoPabaigosData);
+
+            mokinys1.ParodytiDuomenis();
+            mokinys2.ParodytiDuomenis();
+
+            Console.ReadLine();
         }
     }
 }
