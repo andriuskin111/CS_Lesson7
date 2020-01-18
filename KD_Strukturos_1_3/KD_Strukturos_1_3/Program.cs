4f173f7e29c390eb1dadba500ec21d43d18cee76
andriuskin111 <andriuskin@gmail.com>
2020-01-18 20:40:36 +0200

    prideta struktura konstruktorius
    staciakampio ploto skaiciavimo
    ir isvedimo metodas

--- KD_Strukturos_1_3/KD_Strukturos_1_3/Program.cs
+++ KD_Strukturos_1_3/KD_Strukturos_1_3/Program.cs
@@ -10,6 +10,15 @@ namespace KD_Strukturos_1_3
     {
         static void Main(string[] args)
         {
+            Staciakampis staciakampis1 = new Staciakampis(1, 1.2, 3.8);
+            Staciakampis staciakampis2 = new Staciakampis(2, 2.3, 4.6);
+            Staciakampis staciakampis3 = new Staciakampis(3, 3.2, 5.4);
+
+            staciakampis1.ParodytiStaciakampioPlota();
+            staciakampis2.ParodytiStaciakampioPlota();
+            staciakampis3.ParodytiStaciakampioPlota();
+
+            Console.ReadLine();
         }
     }
 }
---
+++ KD_Strukturos_1_3/KD_Strukturos_1_3/Staciakampis.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KD_Strukturos_1_3
+{
+    struct Staciakampis
+    {
+        public int staciakampioNr;
+        public double staciakampioIlgis;
+        public double staciakampioPlotis;
+
+        public Staciakampis(int nr, double ilgis, double plotis)
+        {
+            staciakampioNr = nr;
+            staciakampioIlgis = ilgis;
+            staciakampioPlotis = plotis;
+        }
+
+        public void ParodytiStaciakampioPlota()
+        {
+            Console.WriteLine($"Staciakampio Nr {staciakampioNr} plotas yra: {staciakampioIlgis * staciakampioPlotis}");
+        }
+    }
+
+    
+}
