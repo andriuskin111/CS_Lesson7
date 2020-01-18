5eb1aa3c5c8c5d2f4ebdcdd68324ab14281b78cf
andriuskin111 <andriuskin@gmail.com>
2020-01-19 00:12:25 +0200

    start

---
+++ ND_Strukturos_Advanced_1/ND_Strukturos_Advanced_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ND_Strukturos_Advanced_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
