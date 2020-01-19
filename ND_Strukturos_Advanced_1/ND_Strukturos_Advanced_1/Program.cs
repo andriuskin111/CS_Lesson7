d7728c100a916be9aea43d375ef9f091731f7999
andriuskin111 <andriuskin@gmail.com>
2020-01-19 03:16:56 +0200

    done

--- ND_Strukturos_Advanced_1/ND_Strukturos_Advanced_1/Program.cs
+++ ND_Strukturos_Advanced_1/ND_Strukturos_Advanced_1/Program.cs
@@ -10,6 +10,28 @@ namespace ND_Strukturos_Advanced_1
     {
         static void Main(string[] args)
         {
+            List<int> pirmasTrimestras = new List<int>();
+            List<int> antrasTrimestras = new List<int>();
+            List<int> treciasTrimestras = new List<int>();
+
+            Studentas studentas1 = new Studentas();
+                       
+            Random rnd = new Random();
+            for (int i = 0; i < 11; i++)
+            {
+                pirmasTrimestras.Add(rnd.Next(2, 10));
+                antrasTrimestras.Add(rnd.Next(2, 10));
+                treciasTrimestras.Add(rnd.Next(2, 10));
+            }
+           
+            studentas1.IsvestiPirmoTrimestroVidurki(pirmasTrimestras);
+            studentas1.IsvestiAntroTrimestroVidurki(antrasTrimestras);
+            studentas1.IsvestiTrecioTrimestroVidurki(treciasTrimestras);
+            studentas1.IsvestiMetiniusVidurkius(pirmasTrimestras, antrasTrimestras, treciasTrimestras);
+
+            Console.ReadLine();
+
         }
+
     }
 }
---
+++ ND_Strukturos_Advanced_1/ND_Strukturos_Advanced_1/Studentas.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ND_Strukturos_Advanced_1
+{
+    struct Studentas
+    {
+        public int pirmoTrimestroPazymiai;
+        public int antroTrimestroPazymiai;
+        public int trecioTrimestroPazymiai;
+
+        public Studentas(int pirmasTr, int antrasTr, int treciasTr)
+        {
+            pirmoTrimestroPazymiai = pirmasTr;
+            antroTrimestroPazymiai = antrasTr;
+            trecioTrimestroPazymiai = treciasTr;
+        }
+
+        //public List<int> lstPirmoTrimestroPazymiai
+        //{
+        //    get { return pirmoTrimestroPazymiai; }
+        //    set { pirmoTrimestroPazymiai = value; }
+        //}
+
+        //public List<int> lstAntroTrimestroPazymiai
+        //{
+        //    get { return antroTrimestroPazymiai; }
+        //    set { antroTrimestroPazymiai = value; }
+        //}
+
+        //public List<int> lstTrecioTrimestroPazymiai
+        //{
+        //    get { return trecioTrimestroPazymiai; }
+        //    set { trecioTrimestroPazymiai = value; }
+        //}
+
+        public void IsvestiPirmoTrimestroVidurki(List<int> pirmoTrimestroPazymiai)
+        {
+            double resultatas = 0;
+            foreach (var item in pirmoTrimestroPazymiai)
+            {
+                resultatas += item; 
+            }
+            Console.WriteLine("Pirmo trimestro vidurkis: {0:0.00}", (resultatas / pirmoTrimestroPazymiai.Count));
+        }
+        public void IsvestiAntroTrimestroVidurki(List<int> antroTrimestroPazymiai)
+        {
+            double resultatas = 0;
+            foreach (var item in antroTrimestroPazymiai)
+            {
+                resultatas += item;
+            }
+            Console.WriteLine("Antro trimestro vidurkis: {0:0.00}", (resultatas / antroTrimestroPazymiai.Count));
+        }
+
+        public void IsvestiTrecioTrimestroVidurki(List<int> trecioTrimestroPazymiai)
+        {
+            double resultatas = 0;
+            foreach (var item in trecioTrimestroPazymiai)
+            {
+                resultatas += item;
+            }
+            Console.WriteLine("Trecio trimestro vidurkis: {0:0.00}", (resultatas / trecioTrimestroPazymiai.Count));
+        }
+
+        public void IsvestiMetiniusVidurkius(List<int> pirmoTrimestroPazymiai, List<int> antroTrimestroPazymiai, List<int> trecioTrimestroPazymiai)
+        {
+            double rezultatas1 = 0;
+            double rezultatas2 = 0;
+            double rezultatas3 = 0;
+
+            foreach (var item in pirmoTrimestroPazymiai)
+            {
+                rezultatas1 += item;
+            }
+
+            rezultatas1 = rezultatas1 / pirmoTrimestroPazymiai.Count;
+
+            foreach (var item in antroTrimestroPazymiai)
+            {
+                rezultatas2 += item;
+            }
+
+            rezultatas2 = rezultatas2 / antroTrimestroPazymiai.Count;
+
+            foreach (var item in trecioTrimestroPazymiai)
+            {
+                rezultatas3 += item;
+            }
+
+            rezultatas3 = rezultatas3 / trecioTrimestroPazymiai.Count;
+
+            Console.WriteLine("Metinis vidurkis: {0:0.00}", (rezultatas1 + rezultatas2 + rezultatas3) / 3);
+        }
+    }
+}
