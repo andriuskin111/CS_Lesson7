313543bd5d5d5017f556ca559d1b709e4bff5449
andriuskin111 <andriuskin@gmail.com>
2020-01-19 22:21:59 +0200

    sukurtas rinkeju sarasas
    struktura su id ir balsavimo rezultatu
    metodas statistikai parodyti

--- ND_Strukturos_Advanced_2/ND_Strukturos_Advanced_2/Program.cs
+++ ND_Strukturos_Advanced_2/ND_Strukturos_Advanced_2/Program.cs
@@ -10,6 +10,21 @@ namespace ND_Strukturos_Advanced_2
     {
         static void Main(string[] args)
         {
+            List<Rinkejas> rinkejasPirmaApylinke = new List<Rinkejas>();
+            Rinkejas rinkejas = new Rinkejas();
+
+            Random rnd = new Random();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                rinkejasPirmaApylinke.Add(new Rinkejas(i, Convert.ToBoolean(rnd.Next(2))));
+            }
+
+            rinkejas.ParodytiBalsavimoStatistika(rinkejasPirmaApylinke);
+                     
+            Console.ReadLine();
+            
         }
+       
     }
 }
---
+++ ND_Strukturos_Advanced_2/ND_Strukturos_Advanced_2/Rinkejas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ND_Strukturos_Advanced_2
+{
+    struct Rinkejas
+    {
+        public int id;
+        public bool arPrabalsavo;
+
+        public Rinkejas(int _id, bool prabalsavo)
+        {
+            id = _id;
+            arPrabalsavo = prabalsavo;
+        }
+
+        public void ParodytiBalsavimoStatistika(List<Rinkejas> rinkejas)
+        {
+            foreach (var item in rinkejas)
+            {
+                Console.WriteLine($"Rinkejo ID: {item.id}, Ar prabalsavo: {item.arPrabalsavo}");
+            }
+        }
+    }
+}
