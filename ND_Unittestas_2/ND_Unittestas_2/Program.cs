882fdaeaa48f289a95d365643aa2383f5e8aa770
andriuskin111 <andriuskin@gmail.com>
2020-01-20 23:17:31 +0200

    pridetas metodas palyginti skaiciu

--- ND_Unittestas_2/ND_Unittestas_2/Program.cs
+++ ND_Unittestas_2/ND_Unittestas_2/Program.cs
@@ -10,6 +10,28 @@ namespace ND_Unittestas_2
     {
         static void Main(string[] args)
         {
+            int skaicius = 2;
+
+            Console.WriteLine(PalygintiArSkaiciusLyginis(skaicius));
+
+            Console.ReadLine();
+            
+        }
+
+        public static bool PalygintiArSkaiciusLyginis(int aSkaicius)
+        {
+            bool rezultatas;
+
+            if(aSkaicius % 2 == 0)
+            {
+                rezultatas = true;
+            }
+            else
+            {
+                rezultatas = false;
+            }
+
+            return rezultatas;
         }
     }
 }
